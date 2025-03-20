a32bfac076e2d3b2173a0be498479e95189f62aa
TECHSOSI <gorimu28@gmail.com>
2025-03-20 14:03:08 +0900

    商品情報編集機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,9 +22,24 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
  
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to root_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+    #item.update(item_params)
+    #redirect_to root_path
+  end
+
   private
   def item_params
-    params.require(:item).permit(:image, :product_name, :product_information, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
+    params.require(:item).permit(:image, :existing_image_id, :product_name, :product_information, :category_id, :condition_id, :shipping_cost_id, :prefecture_id, :shipping_date_id, :price).merge(user_id: current_user.id)
   end
 
 end
