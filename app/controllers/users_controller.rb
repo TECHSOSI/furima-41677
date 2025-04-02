7758626116406eb1d6c2fc34a8a9098a5f83ace0
TECHSOSI <gorimu28@gmail.com>
2025-04-03 00:14:12 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,4 +17,5 @@ class UsersController < ApplicationController
     params.require(:user).permit(:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date)
   end
 
+  
 end
